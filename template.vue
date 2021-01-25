8fed0c713b501194ad6a5c23cfef9d120ffd751f
komavideo <koma.endia@gmail.com>
2021-01-25 15:40:05 +0900

    组件模版

---
+++ template.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="text-gray-600">
+            <header class="mb-5">
+                <div class="bg-gray-700 p-4 text-white">你好，Tailwind CSS.</div>
+            </header>
+            <main class="container mx-auto">
+                <h1 class="text-2xl">小马学Tailwind CSS</h1>
+
+
+
+            </main>
+            <footer class="mt-5">
+                <div class="bg-gray-300 p-4 text-black">再见，Bootstrap.</div>
+            </footer>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                list: []
+            };
+        },
+    }
+</script>
